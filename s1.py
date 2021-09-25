d4cd8f32763155a971e7029b8e02d82698ac9129
Amin Hashemi <aminhashemi92@gmail.com>
2021-09-25 20:06:53 +0330

    s1

---
+++ s1.py
@@ -0,0 +1,93 @@
+#print("Hello world!")
+
+#syntax  - Logic - semantic
+
+# variables    int float str bool complex list tuple dic
+#from keyword import iskeyword
+
+
+# message = "Hello World!"
+# n = 17
+# pi = 3.1415
+
+# print(type(message))
+# print(type(n))
+# print(type(pi))
+
+
+#string
+
+# name = "amin hashemi"
+# print(name)
+# print(name.title())
+# print(name.upper())
+# print(name.lower())
+#methods
+
+
+#f-string
+
+# first_name = "parya"
+# last_name = "abouhamzeh"
+# # full_name = f"{first_name} {last_name}"
+# full_name = "{} {}".format(first_name, last_name)
+# print(full_name)
+
+
+
+
+# numbers
+
+# operators : + - * / ** % //
+
+# a = 3 * 2 - 6 / 5 ** 2
+# b = 5 - 3 - 2
+# x = 3
+# x = x + 1
+# x += 1
+# x -= 1
+# x *= 1 
+
+
+# universe_age = 14_000_000_000
+
+# x, y, z = 2, 0, 0
+# print(x)
+
+
+
+# a = "amin"
+# b = "hashemi"
+# print(a + b)
+# print(a*2)
+
+
+# n = input("please enter a number: ")
+# print(type(n))
+
+# casting
+# n = int(n)
+# print(2*n)
+
+
+# n = 12.3456
+# print(n)
+# print('%i' %n)
+# print('%e' %n)
+
+# n = 17.5
+# print("Hello %d world" %n)
+
+
+#Boolean  True False
+# a = False
+# print(type(a))
+
+
+    
+# print(10 % 4.5)
+
+
+
+
+
