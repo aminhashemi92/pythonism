2ecf9245d1bcb3ef0afeee97293aeb164a8819b7
Amin Hashemi <aminhashemi92@gmail.com>
2021-09-27 11:11:49 +0330

    s2

---
+++ s2.py
@@ -0,0 +1,205 @@
+# #List
+# cars  = ["benz", "bmw", "sipa", "toyota"]
+# print(cars)
+
+# cars[2] = "bugatti"
+# print(cars)
+
+# # cars.append('honda')
+# cars.insert(2,"honda")
+# print(cars)
+
+# # del cars[2]
+# # cars.pop(0)
+# # cars.remove('benz')
+# # print(cars)
+
+# cars.sort()
+# print(cars)
+
+# print(len(cars))
+
+# a = [1, "amin", [25,8]]
+# print(a[2][0])
+# print(2 not in a)
+
+# matrix = [[1,2],[3,4]]
+
+
+
+# for
+
+# cars  = ["benz", "bmw", "sipa", "toyota"]
+
+# for car in cars:
+#     print(car)
+# print("salam")
+
+
+
+# print(type(range(1,5)))
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,11,2):
+#     print(i)
+
+# cars  = ["benz", "bmw", "sipa", "toyota"]
+# for i in range(len(cars)):
+#     print(cars[i])
+
+
+
+# a = [1,2,3,4,5]
+# for i in range(len(a)):
+#     a[i] = a[i] * i
+
+# print(a)
+
+
+
+# a = [1,2,3]
+# b = [4]
+# c = b + a
+# # c.sort(reverse=True)
+# # print(4 * a)
+# # print(b + a)
+# print(c)
+
+
+# a = [1,2,3]
+# b = [4,5]
+# a.extend(b)
+# print(a)
+
+
+# squares = []
+# # squares = list()
+# for i in range(1,10):
+#     squares.append(i**2)
+    
+# print(squares)
+# print(sum(squares))
+
+# sum = 0
+# for i in squares:
+#     sum += i
+
+# print(sum)
+
+
+# squares = [i**2 for i in range(10)]
+# # print(dir(squares))
+# print(squares[2:-1])
+
+
+
+# a = [1,2,3]
+# # b = a
+# # b = a.copy()
+# b = a[:]
+# print(b)
+# b[0]=10
+# print(b) 
+# print(a)
+# print(a is b)
+
+
+
+
+
+# name = "amin"
+# # print(name[1:])
+# t = list(name)
+# print(t)
+
+
+# split()
+# s = "aminhashemi@gmail.com"
+# t = s.split("@")
+# print(t)
+
+
+
+
+# t = ['amin', 'hashemi']
+# delimiter = ' '
+# print(delimiter.join(t))
+
+
+# email = "aminhasnhemi92@gmail.com"
+# # a ,b = email.split("@")
+# # print(a)
+# # print(b)
+
+
+# email = email.replace("2", "1")
+# print(email.strip.__doc__)
+
+
+
+# tuples
+
+# dim = (200, 250)
+# print(type(dim))
+# # dim[0] = 50
+# print(dim)
+
+# h = (100,)
+# print(type(h))
+
+
+# for i in dim:
+#     print(i)
+
+# dim = (200, 250)
+# dim = list(dim)
+# print(dim)
+# dim[0] = 50
+# dim = tuple(dim)
+# print(dim)
+
+
+# for i in range(1,10): # i = 1    2    3 
+#     for j in range(1,10): # j = 1,9
+#         print(i*j, end="\t")
+#     print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
